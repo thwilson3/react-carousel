94d3372d7864f0209906a1cea592183f081256d3
Thomas Wilson <tommyhwilson3@gmail.com>
2023-04-18 18:11:00 -0500

    added test for left arrow

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -31,6 +31,45 @@ it("works when you click on the right arrow", function () {
   ).toBeInTheDocument();
 });
 
+it("works when you click on the left arrow", function () {
+  const { container } = render(
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+   // expect the first image to show, but not the second
+   expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+});
+
+
+
 it("matches snapshot", function () {
   const { container } = render(
     <Carousel photos={TEST_IMAGES} title="images for testing" />
